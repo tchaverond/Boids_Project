b55ab702c3c32fa7ff9d67f0f4e3a829afa8bac6
Chaverondier Thomas <tchaverond@bs406-s16-06.insa-lyon.fr>
2015-01-05 14:36:54 +0100

    'Boid' class now follows standard landscape

--- Boid.cpp
+++ Boid.cpp
@@ -1,16 +1,16 @@
-#include <cstdio>
+/*#include <cstdio>
 #include <cstdlib>
 #include "Boid.h"
 #include "Agent.h"
 
 
-Boid :: Boid(void)
+Boid::Boid(void)
 {
   head = new Agent();
   nb_elts = 0;
 }
 
-Boid :: ~Boid(void)
+Boid::~Boid(void)
 {
   Agent* i = new Agent();
   for (i=head; i->get_next()!=NULL; i=i->get_next())
@@ -19,14 +19,10 @@ Boid :: ~Boid(void)
     }
 }
 
-int Boid :: getnb_elts (void)
-{
-
-  return nb_elts;
-}
 
 
-void Boid :: append (Agent* element)
+
+void Boid::append (Agent* element)
 {
   Agent* i = new Agent();
   for (i=head; i->get_next()!=NULL; i=i->get_next())
@@ -37,7 +33,96 @@ void Boid :: append (Agent* element)
   printf("%d\n",nb_elts);
 }
 
-void Boid :: remove (Agent* element)
+void Boid::remove (Agent* element)
+{
+  Agent* i = head;
+  while ((i->get_next()!=element) && (i->get_next()!=NULL))
+    {
+      i = i->get_next();
+    }
+  if (i->get_next()==element)
+    {
+      i->set_next(element->get_next());
+      delete(element);
+      nb_elts--;
+      printf("%d\n",nb_elts);
+    }
+}*/
+
+//****************************************************************************
+//
+//
+//
+//****************************************************************************
+
+
+ 
+ 
+// ===========================================================================
+//                                   Libraries
+// ===========================================================================
+
+
+
+// ===========================================================================
+//                                 Project Files
+// ===========================================================================
+#include "Boid.h"
+#include "Agent.h"
+
+
+
+
+//############################################################################
+//                                                                           #
+//                           Class Boid                          #
+//                                                                           #
+//############################################################################
+
+// ===========================================================================
+//                         Definition of static attributes
+// ===========================================================================
+
+
+// ===========================================================================
+//                                  Constructors
+// ===========================================================================
+
+Boid::Boid(void)
+{
+  head = new Agent();
+  nb_elts = 0;
+}
+
+// ===========================================================================
+//                                  Destructor
+// ===========================================================================
+
+Boid::~Boid(void)
+{
+  Agent* i = new Agent();
+  for (i=head; i->get_next()!=NULL; i=i->get_next())
+    {
+      delete (i);
+    }
+}
+
+// ===========================================================================
+//                                 Public Methods
+// ===========================================================================
+
+void Boid::append (Agent* element)
+{
+  Agent* i = new Agent();
+  for (i=head; i->get_next()!=NULL; i=i->get_next())
+    {}
+  i->set_next(element);
+  element->set_next(NULL);
+  nb_elts++;
+  printf("%d\n",nb_elts);
+}
+
+void Boid::remove (Agent* element)
 {
   Agent* i = head;
   while ((i->get_next()!=element) && (i->get_next()!=NULL))
@@ -52,3 +137,11 @@ void Boid :: remove (Agent* element)
       printf("%d\n",nb_elts);
     }
 }
+
+// ===========================================================================
+//                                Protected Methods
+// ===========================================================================
+
+// ===========================================================================
+//                               Non inline accessors
+// ===========================================================================
